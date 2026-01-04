65bcb89b371c79c850a9351385de3d55278fe59d
algas <algassabk@gmail.com>
2026-01-04 14:36:34 +0300

    git init git add . git commit -m "Step 1 - COMPLETED"

---
+++ src/main/java/com/todoapp/todoapp/Controller/HelloController.java
@@ -0,0 +1,13 @@
+package com.todoapp.todoapp.Controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+
+    @GetMapping("/hello")
+    public String hello() {
+        return "Hello World!";
+    }
+}
